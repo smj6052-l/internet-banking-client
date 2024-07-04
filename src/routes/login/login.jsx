4a5d5f84d805a7d6724a3ff28157131793bea6f5
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-04 22:12:56 +0900

    feat: 로그인 페이지(임시) 추가

--- src/routes/login/login.jsx
+++ src/routes/login/login.jsx
@@ -1,8 +1,28 @@
+import { useState, useRef } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import * as S from "./styles/login.style";
+
 // 로그인 페이지
 export default function Login() {
   return (
     <>
-      <h1>로그인</h1>
+      <S.Container onClick={(e) => e.stopPropagation()}>
+        <S.InnerContainer>
+          <S.Header>
+            <S.Title>로그인</S.Title>
+          </S.Header>
+          <S.FormWrapper>
+            <S.Form>
+              <S.Input />
+              <S.Input />
+              <S.LoginButton type="submit">로그인</S.LoginButton>
+            </S.Form>
+            <S.SignupHeader>아직 계정이 없으신가요?</S.SignupHeader>
+            <S.SignupButton>회원가입하기 &rarr;</S.SignupButton>
+          </S.FormWrapper>
+        </S.InnerContainer>
+      </S.Container>
     </>
   );
 }
