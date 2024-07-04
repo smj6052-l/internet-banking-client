445d42c4b2a491c4f854b0c8fe174b99f49819f8
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-04 17:24:07 +0900

    chore: vite 프록시 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+        ws: true,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
