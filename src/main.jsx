e8e56e0650edb3d42f9c6c5f396b5d9b5e12245d
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-04 21:58:12 +0900

    chore: 전역 스타일링 App에 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.jsx";
+import { GlobalStyle } from "./styles/global-style.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <>
+    <GlobalStyle />
     <RouterProvider router={router} />
   </>
   // </React.StrictMode>
