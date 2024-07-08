94fa592f35af0142c91f748dd9f9c857bc27eda4
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-07-08 23:36:33 +0900

    feat: 계좌 이체 기능 추가

--- src/routes/transfer/transfer.jsx
+++ src/routes/transfer/transfer.jsx
@@ -1,39 +1,149 @@
+import axios from "axios";
 import * as S from "./styles/transfer.style";
 import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
+import { useForm } from "react-hook-form";
+import { useNavigate, useParams } from "react-router-dom";
+import { useState } from "react";
+import useBottomSheet from "../../hooks/useBottomSheet";
+import BottomSheet from "../../components/bottom-sheet";
 
-// 입/출금 페이지
-export default function Transfer() {
-  const options = ["one", "two", "three"];
+// 입/출금 페이지: 본인 계좌에 입금시 받는 계좌 즉시 주입
+export default function Transfer({ destinationAccount }) {
+  const [originAccount, setOriginAccount] = useState("");
+
+  const { onDragEnd, controls, setIsOpen } = useBottomSheet();
+
+  const options = ["3374-16-562652", "two", "three"];
   const defaultOption = options[0];
+
+  const { accountId } = useParams();
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    clearErrors,
+    formState: { errors },
+  } = useForm();
+
+  // POST: 이체
+  const onSubmit = async ({
+    transaction_destination,
+    transaction_amount,
+    transaction_origin_memo,
+    transaction_destination_memo,
+    account_pw,
+  }) => {
+    const transferInfo = {
+      transaction_origin: originAccount,
+      transaction_destination,
+      transaction_amount,
+      transaction_origin_memo,
+      transaction_destination_memo,
+      account_pw,
+    };
+
+    const transferURL = `/api/transaction/${accountId}/transfer`;
+    await axios
+      .post(transferURL, transferInfo, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          alert("이체에 성공했습니다");
+          navigate("/home");
+        }
+      })
+      .catch(() => {
+        alert("계좌 정보를 불러오는데 실패했습니다.");
+      });
+  };
+
+  const changeOriginAccount = (data) => {
+    setOriginAccount(data.value);
+  };
+  // transaction_origin,
   return (
-    <>
-      <S.Wrapper>
-        <S.Container>
-          <S.Header>
-            <S.AccountNumber></S.AccountNumber>
-          </S.Header>
-          <S.TransferAmount placeholder="보낼 금액"></S.TransferAmount>
-          {/* 보내는 계좌 */}
+    <S.Wrapper>
+      <S.FormContainer
+        onClick={(e) => e.stopPropagation()}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        {/* 받는 계좌 번호 */}
+        <S.Header>이체하기</S.Header>
+        {/* 받는 계좌 번호 입력 */}
+        <S.DestinationAccountInput
+          type="text"
+          value={destinationAccount}
+          placeholder="받는사람 계좌번호"
+          {...register("transaction_destination", {
+            required: true,
+            maxLength: 20,
+          })}
+        />
+        <S.TransferAmount
+          placeholder="보낼 금액"
+          {...register("transaction_amount", { required: true, maxLength: 20 })}
+        ></S.TransferAmount>
+        {/* 보내는 계좌 */}
+        <S.OriginAccountSelect>
           <Dropdown
             options={options}
-            // onChange={this._onSelect}
+            onChange={changeOriginAccount}
             value={defaultOption}
             placeholder="Select an option"
           />
-          {/* 표기란 */}
-          <S.TitleContainer>
-            <S.Description>받는 분에게 표기</S.Description>
-            <S.TitleToRecieverInput></S.TitleToRecieverInput>
-          </S.TitleContainer>
-          <S.TitleContainer>
-            <S.Description>나에게 표기</S.Description>
-            <S.TitleToMeInput></S.TitleToMeInput>
-          </S.TitleContainer>
-          {/* 완료 버튼 */}
-          <S.TransferBtn>이체하기</S.TransferBtn>
-        </S.Container>
-      </S.Wrapper>
-    </>
+        </S.OriginAccountSelect>
+        {/* 표기란 */}
+        <S.MemoContainer>
+          <S.Description>받는 분에게 표기</S.Description>
+          <S.MemoInput
+            maxLength={8}
+            {...register("transaction_destination_memo", {
+              required: true,
+              maxLength: 8,
+            })}
+          />
+        </S.MemoContainer>
+        <S.MemoContainer>
+          <S.Description>나에게 표기</S.Description>
+          <S.MemoInput
+            maxLength={8}
+            {...register("transaction_origin_memo", {
+              required: true,
+              maxLength: 8,
+            })}
+          ></S.MemoInput>
+        </S.MemoContainer>
+        <S.NextBtn
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            setIsOpen(true);
+          }}
+        >
+          다음
+        </S.NextBtn>
+        <BottomSheet onDragEnd={onDragEnd} controls={controls}>
+          <S.BtmSheetContainer>
+            <S.AccountPwInput
+              type="password"
+              placeholder="계좌 비밀번호"
+              required
+              {...register("account_pw", {
+                required: true,
+                minLength: 6,
+                maxLength: 6,
+              })}
+            />
+            {/* 완료 버튼 */}
+            <S.TransferBtn type="submit">이체하기</S.TransferBtn>
+          </S.BtmSheetContainer>
+        </BottomSheet>
+      </S.FormContainer>
+    </S.Wrapper>
   );
 }
