1dacea49bc90c64b5f8e1d3a7f53817cc436e4f4
smj6052-l <seominjung6052@gmail.com>
2024-08-19 10:34:44 +0900

    Update vite.config.js
    http

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://backend-server:3000", // HTTPS로 설정된 백엔드 서버의 주소
+        target: "http://backend-server:3000", // HTTPS로 설정된 백엔드 서버의 주소
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
         secure: false,
